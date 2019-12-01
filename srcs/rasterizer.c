9c7918556182dafc18b43084babad45c1f422c66
gedemais <geoffrey.demaiso@gmail.com>
2019-12-01 05:36:56 +0100

    Trying stuff

--- srcs/rasterizer.c
+++ srcs/rasterizer.c
@@ -11,9 +11,10 @@ void	rasterizer(t_env *env)
 {
 	(void)env;
 	unsigned int		i;
-	t_triangle			tri;
-	t_triangle			rtri;
-	static float		theta = 90.0f;
+	static float		theta = 0.0f;
+	t_triangle			t;
+//	t_triangle			translate;
+//	t_triangle			rotate;
 
 	i = 0;
 	t_mesh	mesh;
@@ -71,50 +72,47 @@ void	rasterizer(t_env *env)
 // cube shit
 	while (i < 12)
 	{
-		printf("angle = %f\n", (double)theta);
+		ft_memcpy(&t, &mesh.tris[i], sizeof(t_triangle));
 
 		update_xrotation_matrix(&env->cam, (float)ft_to_radians((double)theta));
+		t.points[0] = multiply_matrix(env->cam.rx_m, t.points[0]);
+		t.points[1] = multiply_matrix(env->cam.rx_m, t.points[1]);
+		t.points[2] = multiply_matrix(env->cam.rx_m, t.points[2]);
 
-		rtri.points[0] = multiply_matrix(env->cam.rx_m, mesh.tris[i].points[0]);
-		rtri.points[1] = multiply_matrix(env->cam.rx_m, mesh.tris[i].points[1]);
-		rtri.points[2] = multiply_matrix(env->cam.rx_m, mesh.tris[i].points[2]);
+		t.points[0].x += 1.0f;
+		t.points[0].y += 1.0f;
+		t.points[0].z += 1.0f;
+		t.points[1].x += 1.0f;
+		t.points[1].y += 1.0f;
+		t.points[1].z += 1.0f;
+		t.points[2].x += 1.0f;
+		t.points[2].y += 1.0f;
+		t.points[2].z += 1.0f;
 
-		rtri.points[0].z += 2.0f;
-		rtri.points[1].z += 2.0f;
-		rtri.points[2].z += 2.0f;
+		t.points[0] = multiply_matrix(env->cam.p_m, t.points[0]);
+		t.points[1] = multiply_matrix(env->cam.p_m, t.points[1]);
+		t.points[2] = multiply_matrix(env->cam.p_m, t.points[2]);
 
-		rtri.points[0].x += 2.0f;
-		rtri.points[1].x += 2.0f;
-		rtri.points[2].x += 2.0f;
+		t.points[0].x += 1.0f;
+		t.points[0].y += 1.0f;
+		t.points[1].x += 1.0f;
+		t.points[1].y += 1.0f;
+		t.points[2].x += 1.0f;
+		t.points[2].y += 1.0f;
 
-		rtri.points[0].y += 2.0f;
-		rtri.points[1].y += 2.0f;
-		rtri.points[2].y += 2.0f;
+		t.points[0].x *= 0.025f * (float)WDT;
+		t.points[0].y *= 0.025f * (float)HGT;
+		t.points[1].x *= 0.025f * (float)WDT;
+		t.points[1].y *= 0.025f * (float)HGT;
+		t.points[2].x *= 0.025f * (float)WDT;
+		t.points[2].y *= 0.025f * (float)HGT;
 
-		tri.points[0] = multiply_matrix(env->cam.p_m, rtri.points[0]);
-		tri.points[1] = multiply_matrix(env->cam.p_m, rtri.points[1]);
-		tri.points[2] = multiply_matrix(env->cam.p_m, rtri.points[2]);
-
-		tri.points[0].x += 1.0f;
-		tri.points[0].y += 1.0f;
-		tri.points[1].x += 1.0f;
-		tri.points[1].y += 1.0f;
-		tri.points[2].x += 1.0f;
-		tri.points[2].y += 1.0f;
-
-		tri.points[0].x *= (float)WDT * 0.05f;
-		tri.points[0].y *= (float)HGT * 0.05f;
-		tri.points[1].x *= (float)WDT * 0.05f;
-		tri.points[1].y *= (float)HGT * 0.05f;
-		tri.points[2].x *= (float)WDT * 0.05f;
-		tri.points[2].y *= (float)HGT * 0.05f;
-
-		draw_triangle(&env->mlx, (t_point){(int)tri.points[0].x, (int)tri.points[0].y},
-					(t_point){(int)tri.points[1].x, (int)tri.points[1].y},
-					(t_point){(int)tri.points[2].x, (int)tri.points[2].y});
+		draw_triangle(&env->mlx, (t_point){(int)t.points[0].x, (int)t.points[0].y},
+					(t_point){(int)t.points[1].x, (int)t.points[1].y},
+					(t_point){(int)t.points[2].x, (int)t.points[2].y});
 		i++;
 	}
-	theta += 0.5f;
+	theta += 0.1f;
 }
 
 
