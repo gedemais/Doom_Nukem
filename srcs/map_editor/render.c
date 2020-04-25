af277843a50eb35f135615ec211a941b5db6f944
Geoffrey Demaison <geoffrey.demaiso@gmail.com>
2020-04-25 15:18:26 +0200

    pre-merge map_editor maboye

--- srcs/map_editor/render.c
+++ srcs/map_editor/render.c
@@ -6,7 +6,7 @@
 /*   By: gedemais <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 02:51:39 by gedemais          #+#    #+#             */
-/*   Updated: 2020/04/25 02:16:24 by gedemais         ###   ########.fr       */
+/*   Updated: 2020/04/25 15:17:55 by gedemais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,7 @@
 
 static int				handle_events_me(t_env *env)
 {
-	static bool	clic = false;
-	int			i;
-
-	//configurer pour switch context du button 
-	i = -1;
-	if (env->events.keys[KEY_ESCAPE])
-		exit(EXIT_SUCCESS);
-	while (++i < EDT_BUTTON_MAX)
-		if (is_on_button(env->events.mouse_pos, env->edit_env.buttons[i]))
-		{
-			if (!env->events.buttons[BUTTON_LCLIC] && clic)
-			{
-				switch_context(env, env->edit_env.buttons[i].context);
-				clic = false;
-				return (1);
-			}
-			clic = env->events.buttons[BUTTON_LCLIC];
-			env->edit_env.buttons[i].is_hover = true;
-		}
-		else
-			env->edit_env.buttons[i].is_hover = false;
+	(void)env;
 	return (0);
 }
 
@@ -64,18 +44,7 @@ int		render_maped(void *param)
 
 	env = ((t_env*)param);
 
-/********************************************/
-/********************************************/
-
 	handle_events_me(env);
-	
-	
-	map_sprite(env->mlx.img_data, env->sprites[SP_EDT_BGD], (t_point){0, 0});
-	map_sprite(env->mlx.img_data, env->sprites[SP_EDT_ME_TITLE], (t_point){328, 370});
-	map_sprite(env->mlx.img_data, env->sprites[SP_ME_RECT_BTN], (t_point){328, 430});
-	map_sprite(env->mlx.img_data, env->sprites[SP_ME_RECT_PRW], (t_point){751, 506});
-	//faire un sous context pour le render
-	render_buttons_me(env);
 
 	input_field(env, (t_point){300, 300}, NULL);
 	mlx_put_image_to_window(env->mlx.mlx_ptr, env->mlx.mlx_win, env->mlx.img_ptr, 0, 0);
