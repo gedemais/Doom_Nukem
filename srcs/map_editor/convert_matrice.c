91ed5c2e146e9817e78c17d683333dd02d51a5b6
maboye <maboye@maboyes-iMac.local>
2020-05-08 06:46:17 -0700

    norme import

--- srcs/map_editor/convert_matrice.c
+++ srcs/map_editor/convert_matrice.c
@@ -1,20 +1,4 @@
 #include "main.h"
-/*
-static void		print_matrice(t_ed_map *env, int len)
-{
-	int		i;
-
-	i = 0;
-	while (i < len)
-	{
-		if (env->flat[i] != 126)
-			printf("0");
-		else
-			printf("\n");
-		i++;
-	}
-	printf("------------------------------------------\n");
-}*/
 
 static void		copy_matrice(t_ed_map *env, int *len)
 {
@@ -31,12 +15,12 @@ static void		copy_matrice(t_ed_map *env, int *len)
 			while (++nb[3] < env->depth)
 				env->flat[nb[0]++] = env->map[nb[1]][nb[2]][nb[3]];
 			if (nb[2] + 1 < env->height)
-				env->flat[nb[0]++] = 126;
+				env->flat[nb[0]++] = 127;
 		}
 		if (nb[1] + 1 < env->width)
 		{
-			env->flat[nb[0]++] = 126;
-			env->flat[nb[0]++] = 126;
+			env->flat[nb[0]++] = 127;
+			env->flat[nb[0]++] = 127;
 		}
 	}
 	env->flat[nb[0]] = '\0';
@@ -54,56 +38,65 @@ int			flat_map(t_ed_map *env, int *len)
 	if (!(env->flat = (char *)ft_memalloc((sizeof(char) * (size + 1)))))
 		return (0);
 	copy_matrice(env, len);
-	//print_matrice(env, *len);
 	return (1);
 }
 
-/************************************************************/
+static int		handle_backline(t_ed_map *env, int *nb, int len)
+{
+	++nb[2];
+	if (env->width == 0)
+		env->width = nb[0] - nb[1];
+	else if (env->width != nb[0] - nb[1])
+		return (0);
+	if (env->flat[nb[0] + 1] == 127)
+	{
+		if (env->height == 0)
+			env->height = nb[2];
+		else if (env->height != nb[2])
+			return (0);
+		nb[2] = 0;
+		++nb[3];
+		++nb[0];
+	}
+	return (nb[0] + 1 != len);
+}
+
+static int		handle_endline(t_ed_map *env, int *nb)
+{
+	++nb[2];
+	++nb[3];
+	if (env->width != nb[0] - nb[1])
+		return (0);
+	if (env->height == 0)
+		env->height = nb[2];
+	if (env->depth == 0)
+		env->depth = nb[3];
+	return (env->height == nb[2]);
+}
 
 static int		get_matrice_size(t_ed_map *env, int len)
 {
-	int		x;
-	int		y;
-	int		z;
-	int		i;
+	int nb[4];
 
-	i = 0;
-	x = 0;
-	y = 0;
-	z = 0;
-	while (i < len)
+	nb[2] = 0;
+	nb[3] = 0;
+	nb[0] = -1;
+	while (++nb[0] < len)
 	{
-		while (i < len && env->flat[i] != 126)
+		nb[1] = nb[0];
+		while (nb[0] < len && env->flat[nb[0]] != 127)
+			++nb[0];
+		if (env->flat[nb[0]] == 127)
 		{
-			i++;
-			x++;
-		}
-		if (env->flat[i] == 126 && i < len - 1 && env->flat[i + 1] != 126)
-		{
-			if (env->width == 0)
-				env->width = x;
-			else if (env->width != x)
+			if (handle_backline(env, nb, len) == 0)
 				return (0);
-			x = 0;
-			z++;
 		}
-		else if (env->flat[i] == 126 && i < len - 1 && env->flat[i + 1] == 126)
+		else if (nb[0] == len)
 		{
-			if (env->depth == 0)
-				env->depth = z;
-			else if (env->depth != z)
+			if (handle_endline(env, nb) == 0)
 				return (0);
-			x = 0;
-			z = 0;
-			y++;
-			i++;
 		}
-		else if (i != len)
-			return (0);
-		i++;
 	}
-	env->depth++;
-	env->height = y + 1;
 	return (env->width && env->height && env->depth);
 }
 
@@ -124,7 +117,7 @@ static void		stock_matrice(t_ed_map *env)
 			d = -1;
 			while(++d < env->depth)
 			{
-				while (env->flat[i] == 126)
+				while (env->flat[i] == 127)
 					++i;
 				env->map[w][h][d] = env->flat[i];
 				++i;
@@ -137,7 +130,6 @@ int				flat_to_matrice(t_ed_map *env, int offset, int len)
 {
 	env->flat += offset;
 	len -= offset;
-	//print_matrice(env, len);
 	if (env->flat == NULL
 		|| get_matrice_size(env, len) == 0 
 		|| init_matrice(env) == 0)
@@ -147,9 +139,5 @@ int				flat_to_matrice(t_ed_map *env, int offset, int len)
 	}
 	stock_matrice(env);
 	env->flat -= offset;
-//	print_matrice(env, len);
 	return (1);
 }
-
-
-
