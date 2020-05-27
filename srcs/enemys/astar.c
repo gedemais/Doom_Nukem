97d7aaf61341645a9ebbb970fd49b0794ee12628
gedemais <marcaboye@gmail.com>
2020-05-27 17:26:26 +0200

    premice astar

--- srcs/enemys/astar.c
+++ srcs/enemys/astar.c
@@ -46,20 +46,21 @@ static int      astar_exit(t_pf *env, t_node *current)
         || env->d_astar.nb_cells < 1);
 }
 
-static void     astar_solve(t_pf *env, t_node **c)
+static int      astar_solve(t_pf *env, t_node **c)
 {
     int     i;
 
     astar_sort_dynarray(&env->d_astar);
     astar_delvisited_nodes(&env->d_astar);
-    *(c) = env->d_astar.c;
+    *c = env->d_astar.c;
     *(c)->bvisited = 1;
     env->nodes[(*c)->x][(*c)->y][(*c)->z].bvisited = 1;
     if (astar_exit(env, *c))
-        return ;
+        return (1);
     i = -1;
     while (++i < NEIGHBOURG)
         astar_neighbour(env, c, i);
+    return (0);
 }
 
 void            astar(t_pf *env)
@@ -72,5 +73,8 @@ void            astar(t_pf *env)
     current->localgoal = 0;
     current->globalgoal = astar_distance(*env->start, *env->end);
     while (1)
-        astar_solve(env, &current);
+    {
+        if (astar_solve(env, &current))
+            return ;
+    }
 }
