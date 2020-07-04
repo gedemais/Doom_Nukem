375b68b26cbe7bbd17c7019625f36c2bb0686447
maboye <maboye@maboyes-iMac.local>
2020-07-04 08:13:42 -0700

    maboye

--- srcs/custom_mode/routines.c
+++ srcs/custom_mode/routines.c
@@ -30,6 +30,7 @@ int				custom_play_to_menu(t_env *env)
 	sound_system(env, SA_TITLE_SCREEN_L,
 		sp_overall(0, SA_MAX, sp_stop()));
 	free_dynarray(&env->custom_env.mobs);
+	free_dynarray(&env->custom_env.loots);
 	free_dynarray(&env->astar.d_nodes);
 	free_dynarray(&env->astar.d_astar);
 	// free new_map
