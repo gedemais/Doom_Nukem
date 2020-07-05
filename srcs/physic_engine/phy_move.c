4f3cd038c94924e326bc9ddd1781cc145674a79a
Benji <benjamin.bosson@gmail.com>
2020-07-05 19:52:24 +0200

    saut sur space

--- srcs/physic_engine/phy_move.c
+++ srcs/physic_engine/phy_move.c
@@ -30,10 +30,10 @@ void	phy_move(t_env *env, bool keys[NB_KEYS], t_map *maps) //2
 	if (env->cam.stats.onfloor == 1 || env->cam.stats.onwall == 1)
 		f = set_y_dir(env, maps); // a virer dans engine ! 
 	r = vec_fdiv((t_vec3d){f.z, 0, -f.x, 0}, env->cam.stats.aspect_ratio);
-	if ((keys[KEY_W] || keys[KEY_S] || keys[KEY_A] || keys[KEY_D]|| keys[KEY_E]) && env->cam.stats.onfloor == 1)
+	if ((keys[KEY_W] || keys[KEY_S] || keys[KEY_A] || keys[KEY_D]|| keys[KEY_SPACE]) && env->cam.stats.onfloor == 1)
 	{
 		f = phy_handle_key(env, f, r, keys);
-		if (keys[KEY_E] && env->cam.stats.onroof == 0)
+		if (keys[KEY_SPACE] && env->cam.stats.onroof == 0)
 			f.y += 0.3;
 		if (env->cam.stats.onwall == 1)
 			f = test_dist_wall(env, maps->cam_wall, f);
