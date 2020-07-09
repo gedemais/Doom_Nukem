fddffcbc99e27253c8ac45ad2ae067725b6f7030
Lise <grudler@MBP-de-Grudler.home>
2020-07-09 13:10:45 +0200

    add physic statut on hud

--- srcs/hud/hud.c
+++ srcs/hud/hud.c
@@ -6,11 +6,28 @@
 /*   By: grudler <grudler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/14 16:11:40 by grudler           #+#    #+#             */
-/*   Updated: 2020/07/04 16:15:39 by gedemais         ###   ########.fr       */
+/*   Updated: 2020/07/09 13:06:25 by grudler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+static int	draw_physic(t_env *env)
+{
+	t_ttf_config	*conf;
+	char *str;
+
+	conf = ttf_config();
+	conf->size = 20;
+	if (env->phy_env.type_move == true && !(str = ft_strjoin("Physic : ", "On")))
+		return (-1);
+	else if (env->phy_env.type_move == false && !(str = ft_strjoin("Physic : ", "Off")))
+		return (-1);
+	ft_strcpy((char*)conf->s, str);
+	free(str);
+	my_string_put(env, env->mlx.img_data, (t_point){50, 500}, FONT_COOLVETICA);
+	return (0);
+}
 
 static int	draw_wave(t_env *env)
 {
@@ -39,6 +56,7 @@ int		draw_hud(t_env *env)
 	draw_health(env);
 	draw_compass(env);
 	draw_wave(env);
+	draw_physic(env);
 	return(0);
 
 }
